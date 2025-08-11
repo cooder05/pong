edefbe798e64f89e09897c8f3f6f8c9ac6189a53
sarvesh <sarvesh2005@gmail.com>
2025-08-11 14:11:59 +0530

    ball speed modified

--- pong2.py
+++ pong2.py
@@ -1,4 +1,4 @@
-import pygame,math,time
+import pygame,math,random
 from sys import exit
 
 pygame.init()
@@ -14,9 +14,9 @@ view ="start"
 class paddle():
     def __init__(self, player = False):
         
-        self.y_offset = screen_height/10
-        self.pos = [(screen_width-20)*(not player),screen_height/2 - self.y_offset]
-        self.rect = pygame.Rect(self.pos,(20,screen_height/5))
+        self.y_offset = screen_height/20
+        self.pos = [10+(screen_width-30)*(not player),screen_height/2 - self.y_offset]
+        self.rect = pygame.Rect(self.pos,(10,screen_height/5))
         self.speed = 5
         self.player = player
 
@@ -33,7 +33,7 @@ class paddle():
             if ball_pos [1] - self.y_offset >= 0 and ball_pos[1] + self.y_offset <= screen_height and ball_pos[0] >= screen_width/2:
                 self.pos[1] += (ball_pos[1] - self.rect.centery)*0.04
 
-        self.rect = pygame.Rect(self.pos,(20,screen_height/5))
+        self.rect = pygame.Rect(self.pos,(10,screen_height/5))
 
     def draw(self,surface):
         pygame.draw.rect(surface,(225,225,225),self.rect)
@@ -45,8 +45,8 @@ class ball():
         # abd just update the rect pos directly
         #self.pos = [screen_width/2,screen_height/2]
         self.rect = pygame.Rect((screen_width/2,screen_height/2),(20,20))
-        self.speed_x = -4
-        self.speed_y = 4
+        self.speed_x = random.randint(1,4)
+        self.speed_y = random.randint(-4,4)
 
     def move(self,paddles):
         global s1,s2,view
@@ -72,10 +72,14 @@ class ball():
         newpos = newpos.move(self.speed_x,0)
         if newpos.colliderect(paddles[0]) :
             newpos.left = paddles[0].right
-            self.speed_x *= -1
+            self.speed_x *= -1.1
+            self.speed_x %= 10
+            self.speed_x = math.floor(self.speed_x)
         if newpos.colliderect(paddles[1]) :
             newpos.right = paddles[1].left
-            self.speed_x *= -1
+            self.speed_x *= -1.1
+            self.speed_x %= -10
+            self.speed_x = math.floor(self.speed_x)
         
         self.rect = newpos
         
@@ -97,10 +101,11 @@ class btn:
         self.surf = test_font.render(self.text,True,(255,255,255),(100,100,255))
         self.rect = self.surf.get_rect(center = pos)
         self.hover()
+        pygame.draw.rect(surface,(255,255,255),self.rect.inflate(10,10),5)
         surface.blit(self.surf,self.rect)
 
     def hover(self):
-        self.rect.y = math.floor(screen_height-self.rect.h)/2 +self.wave[1]
+        self.rect.y += self.wave[1]
         self.wave = self.wave.rotate(5) 
 
 
